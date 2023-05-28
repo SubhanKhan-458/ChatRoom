b3d85d1dbbaf68716172a954b7d50ca83761caf3
subhankhan-458 <codephobia458@gmail.com>
2023-05-28 21:13:16 +0500

    Added frontend login component with jwt

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { JwtModule } from '@auth0/angular-jwt';
+// import { createTokenForExternalReference } from '@angular/compiler/src/identifiers';
+
+export function tokenGetter() {
+  return localStorage.getItem("nestjs_chat_app");
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +22,13 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:3000']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
