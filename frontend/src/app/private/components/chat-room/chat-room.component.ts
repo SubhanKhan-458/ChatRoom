19a937f38125280e053fb4587abdcac9a8d3e169
subhankhan-458 <codephobia458@gmail.com>
2023-06-09 17:07:38 +0500

    Sorted messages and left and right side for multiple users implimented

--- frontend/src/app/private/components/chat-room/chat-room.component.ts
+++ frontend/src/app/private/components/chat-room/chat-room.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { MessagePaginateI } from 'src/app/model/message.interface';
 import { RoomI } from 'src/app/model/room.interface';
 import { ChatService } from '../../services/chat-service/chat.service';
@@ -10,24 +11,35 @@ import { ChatService } from '../../services/chat-service/chat.service';
   styleUrls: ['./chat-room.component.scss']
 })
 export class ChatRoomComponent implements OnInit, OnChanges, OnDestroy {
+
   @Input() chatRoom: RoomI;
-  messages$: Observable<MessagePaginateI> = this.chatService.getMessages();
+
+  messages$: Observable<MessagePaginateI> = this.chatService.getMessages().pipe(
+    map((messagePaginate: MessagePaginateI) => {
+      const items = messagePaginate.items.sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime());
+      messagePaginate.items = items;
+      return messagePaginate;
+    })
+  );
+
   chatMessage: FormControl = new FormControl(null, [Validators.required]);
+
   constructor(private chatService: ChatService) { }
   ngOnInit(): void {
   }
 
   ngOnChanges(changes: SimpleChanges) {
     this.chatService.leaveRoom(changes['chatRoom'].previousValue);
-    if(this.chatRoom) {
+    if (this.chatRoom) {
       this.chatService.joinRoom(this.chatRoom);
     }
   }
   ngOnDestroy() {
     this.chatService.leaveRoom(this.chatRoom);
   }
+
   sendMessage() {
-    this.chatService.sendMessage({text: this.chatMessage.value, room: this.chatRoom});
+    this.chatService.sendMessage({ text: this.chatMessage.value, room: this.chatRoom });
     this.chatMessage.reset();
   }
 }
